9a0318b305a33efb5985694d085d4a42dd8dabb9
Tyler-Melton <145461207+Tyler-Melton@users.noreply.github.com>
2023-10-21 12:46:58 -0400

    Servers Security Group
    Added Servers Security Group w/ No inbound rules and allow all outbound

--- main.tf
+++ main.tf
@@ -20,4 +20,17 @@ resource "aws_instance" "windows_server" {
   tags = {
     Name = "Example_TerraformServer"
   }
+
+}
+
+resource "aws_security_group" "Domain" {
+  name    = "Servers"
+  ingress = []
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
